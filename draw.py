77d7c59927754ca1a141f9b46f637693450b028e
zainkhan-afk <zain.9496@gmail.com>
2022-12-07 14:53:29 +0500

    Added aesthetic changes to the drawing function

--- draw.py
+++ draw.py
@@ -11,26 +11,41 @@ class Draw:
 	def clear_canvas(self):
 		self.canvas = np.zeros((self.height, self.width, 3)).astype("uint8")
 
+	def get_planet_mask(self, planet, planet_mask):
+		x = int(planet.pos.x)
+		y = int(planet.pos.y)
+
+		cv2.circle(planet_mask, (x, y), planet.radius, (255, 255, 255), -1)
+
+		return planet_mask
+
+	def get_planet_tail(self, planet, planet_tail_mask):
+		x = int(planet.pos.x)
+		y = int(planet.pos.y)
+
+		cv2.polylines(planet_tail_mask, [np.array(planet.tail)], False, 255, 1)
+
+		return planet_tail_mask
+
 	def draw_planets(self, planets):
 		for planet in planets:
 			x = int(planet.pos.x)
 			y = int(planet.pos.y)
 
-			cv2.circle(self.canvas, (x, y), planet.radius, planet.color, -1)
-			cv2.circle(self.canvas, (x, y), planet.radius, (255, 255, 255), 1)
+			cv2.circle(self.canvas, (x, y), planet.radius+2, planet.color, -1)
+			cv2.polylines(self.canvas, [np.array(planet.tail)], False, planet.color, 3)
+
+		self.canvas = cv2.GaussianBlur(self.canvas,(17,17),0)
+
+		for planet in planets:
+			x = int(planet.pos.x)
+			y = int(planet.pos.y)
+
+			cv2.circle(self.canvas, (x, y), planet.radius, planet.color, 1)
 			cv2.polylines(self.canvas, [np.array(planet.tail)], False, planet.color, 1)
 
-			# ang = planet.acc.angle
-			# x2 = int(np.cos(ang)*15)+x
-			# y2 = int(np.sin(ang)*15)+y
 
-			# cv2.line(self.canvas, (x, y), (x2, y2), (255, 0, 0), 2)
 
-			# ang = planet.vel.angle
-			# x2 = int(np.cos(ang)*15)+x
-			# y2 = int(np.sin(ang)*15)+y
-
-			# cv2.line(self.canvas, (x, y), (x2, y2), (0, 255, 0), 2)
 
 	def render(self):
 		cv2.imshow("canvas", self.canvas)
