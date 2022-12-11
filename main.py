aa86ea3293158003469fd7d9a73112567e5c2638
zainkhan-afk <zain.9496@gmail.com>
2022-12-12 02:37:30 +0500

    Added planet collisions and improved the physics

--- main.py
+++ main.py
@@ -8,23 +8,31 @@ from physics import Physics
 all_planets = []
 
 
-p = Planet(x = WIDTH//2, y = HEIGHT//2, mass = 10000)
+p = Planet(x = WIDTH//2, y = HEIGHT//2, mass = 1000)
 p.radius = 30
 all_planets.append(p)
-# p = Planet(x = WIDTH//3, y = HEIGHT//2, mass = 100)
-# p.radius = 10
-# all_planets.append(p)
+
 for i in range(NUM_PLANETS):
 	p = Planet()
 	p.randomize_acceleration()
 	all_planets.append(p)
 
+# p = Planet(x = WIDTH//2, y = HEIGHT//2)
+# all_planets.append(p)
+
+# p = Planet(x = WIDTH//2, y = HEIGHT//2 - 30)
+# all_planets.append(p)
+
+# p = Planet(x = WIDTH//2, y = HEIGHT//2 + 60)
+# all_planets.append(p)
+
 renderer = Draw(WIDTH, HEIGHT)
 physics_solver = Physics()
 while True:
+	# print()
 	renderer.clear_canvas()
 
-	physics_solver.solve_physics(all_planets)
+	all_planets = physics_solver.solve_physics(all_planets)
 
 	renderer.draw_planets(all_planets)
 	k = renderer.render()
---
+++ stable_orbit1.py
@@ -0,0 +1,37 @@
+from planet import Planet
+from draw import Draw
+from settings import *
+from physics import Physics
+
+
+renderer = Draw(WIDTH, HEIGHT)
+physics_solver = Physics()
+
+all_planets = []
+
+
+p1 = Planet(x = WIDTH//2 + 70, y = HEIGHT//2, mass = 1000)
+p1.radius = 10
+p1.tail_size = 400
+
+p2 = Planet(x = WIDTH//2 - 70, y = HEIGHT//2, mass = 1000)
+p2.radius = 10
+p2.tail_size = 400
+
+acc_y = 150
+
+p1.set_acceleration(x = 0, y =  acc_y)
+p2.set_acceleration(x = 0, y = -acc_y)
+
+all_planets.append(p1)
+all_planets.append(p2)
+
+while True:
+	renderer.clear_canvas()
+
+	all_planets = physics_solver.solve_physics(all_planets)
+
+	renderer.draw_planets(all_planets)
+	k = renderer.render()
+	if k == ord("q"):
+		break
